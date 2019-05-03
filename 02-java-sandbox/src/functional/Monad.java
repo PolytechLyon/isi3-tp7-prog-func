23b5417f5c4f5acc3379f8245c45cfcb1203f9e8
Philippe Charrière <ph.charriere@gmail.com>
2019-05-03 08:42:55 +0200

    Update Monad.java

--- 02-java-sandbox/src/functional/Monad.java
+++ 02-java-sandbox/src/functional/Monad.java
@@ -1,4 +1,4 @@
-package functional;
+package functional.solution;
 
 import java.util.function.Function;
 
@@ -9,14 +9,16 @@ public class Monad<T> {
     return this.value;
   }
 
-  public  Monad(T value) {
+  public Monad(T value) {
     this.value = value;
   }
 
   public <R> Monad<R> map(Function<T,R> f) {
+    return new Monad<R>(f.apply((T) this.getValue()));
   }
 
   public <R> Monad<R> flatMap(Function<T, Monad<R>> f) { // comme le bind
+    return f.apply((T) this.getValue());
   }
 
 }
